90ea7af0b2930fdfa51ce28128191d589b74618e
Vitoria Berto <mvitoriaberto2@gmail.com>
2022-09-21 22:46:47 -0300

    Inicio da estilização da pagina principal

--- MeuSite/templates/principal.php
+++ MeuSite/templates/principal.php
@@ -1,21 +1,25 @@
 
 <section class='divisao' id='noticias'>
 <h1><a href="?pg=templates/content-games/noticias">Noticias</a></h1>
-<img src="static/images/noticias/loud-bicampea.png" alt="" width='500px'>
-<h3>Valorant: Loud é campeã do Mundial na Turquia em cima da OpTic</h3>
+<ul>
+    <img src="static/images/noticias/loud-bicampea.png" alt="" width='400px'>
+    <li><h3>Valorant: Loud é campeã do Mundial </br> na Turquia em cima da OpTic</h3></li>
+    
+</ul>
+
 </section>
 
 
 <section class="divisao" id='jogos-gratuitos'>
 <h1><a href="?pg=templates/content-games/jogos">Jogos Gratuitos</a></h1>
-<img src="static/images/jogos-gratuitos/spirit-of-the-north.png" alt="" width='500px'>
+<img src="static/images/jogos-gratuitos/spirit-of-the-north.png" alt="" width='350px'>
 <h3>Spirit of the North - Epic Games</h3>
 </section>
 
 
 <section class="divisao" id='nossos-jogos'>
 <h1><a href="?pg=templates/content-games/desenvolvimento">Nossos Jogos</a></h1>
-<img src="static/images/desenvolvimento/DinoPlanet.png" alt="" width='500px'>
+<img src="static/images/desenvolvimento/DinoPlanet.png" alt="" width='350px'>
 <h3>Dino Planet</h3>
 </section>
 
